d8f48d4e8f2e1fffbdaca3636dca381010e92a6a
Halvor Ødegård Teigen <halvorot@gmail.com>
2026-07-01 19:27:26 +0200

    Refactor init-data.sh to use parameterized queries for user creation and privilege grants

--- apps/n8n/init-data.sh
+++ apps/n8n/init-data.sh
@@ -3,10 +3,21 @@ set -e;
 
 
 if [ -n "${POSTGRES_NON_ROOT_USER:-}" ] && [ -n "${POSTGRES_NON_ROOT_PASSWORD:-}" ]; then
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-		CREATE USER ${POSTGRES_NON_ROOT_USER} WITH PASSWORD '${POSTGRES_NON_ROOT_PASSWORD}';
-		GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_NON_ROOT_USER};
-		GRANT CREATE ON SCHEMA public TO ${POSTGRES_NON_ROOT_USER};
+	psql \
+		-v ON_ERROR_STOP=1 \
+		-v app_user="$POSTGRES_NON_ROOT_USER" \
+		-v app_password="$POSTGRES_NON_ROOT_PASSWORD" \
+		-v app_database="$POSTGRES_DB" \
+		--username "$POSTGRES_USER" \
+		--dbname "$POSTGRES_DB" <<-'EOSQL'
+		DO
+		$$
+		BEGIN
+			EXECUTE format('CREATE USER %I WITH PASSWORD %L', :'app_user', :'app_password');
+			EXECUTE format('GRANT ALL PRIVILEGES ON DATABASE %I TO %I', :'app_database', :'app_user');
+			EXECUTE format('GRANT CREATE ON SCHEMA public TO %I', :'app_user');
+		END
+		$$;
 	EOSQL
 else
 	echo "SETUP INFO: No Environment variables given!"
